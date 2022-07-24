4f0894d1804c394a9b4918abb164ec7cdd4b5bef
amerfawzy <95379745+amerfawzy@users.noreply.github.com>
2022-07-25 01:26:18 +0200

    Add files via upload

---
+++ session 2 AI.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('love' < 'zebra')
+print('love' < 'long')
+print('love'!= 'long')
+
+
+# In[4]:
+
+
+print('counter' < 'counter')
+
+
+# In[4]:
+
+
+x=str(input("enter your name"))
+y=len(x)
+print(y)
+
+
+# In[12]:
+
+
+name=input("enter your name")
+def name(name):
+    name=str(input("enter your name"))
+    y=len(name)
+    return name
+
+
+# In[5]:
+
+
+name= input('enter your name')
+
+
+# In[9]:
+
+
+z =
+print(int(z))
+
+
+# In[4]:
+
+
+type(name)
+
+
+# In[10]:
+
+
+str=5
+print(str)
+
+
+# In[15]:
+
+
+x='amer'
+print('a'in x)
+print('am'in x)
+print('ame'in x)
+print('amx'in x)
+print('amet'not in x)
+
+
+# In[17]:
+
+
+salary=int(input())
+if salary<1000 :
+    print('you are poor')
+else:
+    print("you are rich")
+    
+print("work hard")
+
+
+# In[19]:
+
+
+x=int(input("enter your salary"))
+if x<1000:
+    print("poor")
+elif 1000<x<20000:
+    print('good salary')
+elif 20000<x<50000:
+    print('rich')
+else:
+    print("3la 7sb")
+        
+
+
+# In[ ]:
+
+
+
+
