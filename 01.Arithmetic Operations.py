9395749eef7bc3bee948b7fd27b71e0c679eff36
amerfawzy <95379745+amerfawzy@users.noreply.github.com>
2022-07-27 19:31:34 +0200

    Add files via upload

---
+++ 01.Arithmetic Operations.py
@@ -0,0 +1,398 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 6
+y = 3
+
+
+# In[2]:
+
+
+print(x + y)            # 9
+
+
+# In[4]:
+
+
+print(x + 2 * y - 1)    # 11
+
+
+# In[5]:
+
+
+print(x + 2 * y - 1)    # 11
+
+
+# In[6]:
+
+
+print(x / y)            # 2.0
+
+
+# In[7]:
+
+
+print(7)        # 7
+
+
+# In[8]:
+
+
+print(+7)       # 7
+
+
+# In[9]:
+
+
+print(+++7)     # 7
+
+
+# In[10]:
+
+
+print(-7)        # -7
+
+
+# In[11]:
+
+
+print(--7)       # 7
+
+
+# In[12]:
+
+
+print(---7)      # -7
+
+
+# In[13]:
+
+
+print(7 -- 5)       # 12
+
+
+# In[14]:
+
+
+print(7 --- 5)       # 2
+
+
+# In[15]:
+
+
+print(14 / 3)   # 4.666666666666667
+
+
+# In[16]:
+
+
+print(14 / 40)  # 0.35
+
+
+# In[17]:
+
+
+print(14 // 2)   # 7
+
+
+# In[18]:
+
+
+print(14 // 40)  # 0
+
+
+# In[19]:
+
+
+print(14 / 8)     # 1.75
+
+
+# In[20]:
+
+
+print(-14 // 8)   # -2  = -2 = min(-2, -1)
+
+
+# In[21]:
+
+
+print(type(14 / 7))     # float
+
+
+# In[22]:
+
+
+print(type(14 // 7))    # int
+
+
+# In[24]:
+
+
+num = 12345
+
+
+# In[25]:
+
+
+print(num / 10)         #  1234.5
+
+
+# In[26]:
+
+
+print(num // 10)        # 1234
+
+
+# In[27]:
+
+
+print(2 ** 4)       # 16
+
+
+# In[28]:
+
+
+print(2 ** -4)      # 0.0625 = 1/16
+
+
+# In[29]:
+
+
+print(2.1 ** 4)     # 19.448100000000004
+
+
+# In[30]:
+
+
+num, k = 12345, 3
+print(num // (10 ** k)) # 12
+
+
+# In[31]:
+
+
+print('Hi')
+print()
+print("I am amer")
+print(3 * 7)
+print("good code")
+print("good \" code")
+
+
+# In[32]:
+
+
+#extra examples:-
+#===========================
+#***************************
+
+
+# In[33]:
+
+
+2=='2'
+
+
+# In[34]:
+
+
+True== 'True'
+
+
+# In[35]:
+
+
+False == 0
+
+
+# In[36]:
+
+
+True == 1
+
+
+# In[37]:
+
+
+True == 2
+
+
+# In[38]:
+
+
+True and False
+
+
+# In[39]:
+
+
+True or False
+
+
+# In[40]:
+
+
+not False
+
+
+# In[41]:
+
+
+not (True == 2)
+
+
+# In[42]:
+
+
+(False == 0) and (True == 1)
+
+
+# In[43]:
+
+
+2 and 1
+
+
+# In[44]:
+
+
+2 or 1 
+
+
+# In[45]:
+
+
+not 4
+
+
+# In[47]:
+
+
+not 0
+
+
+# In[48]:
+
+
+2 and 0 
+
+
+# In[49]:
+
+
+0 and 2 
+
+
+# In[50]:
+
+
+'google' and 1
+
+
+# In[51]:
+
+
+'' and 'go'
+
+
+# In[52]:
+
+
+False or 0
+
+
+# In[53]:
+
+
+x=1.5
+y=2
+print(x+y)
+
+
+# In[54]:
+
+
+x-7
+
+
+# In[62]:
+
+
+a = 1
+b = 2
+
+
+# In[63]:
+
+
+if a >= b:
+    print('true')
+else:
+    print('flase')
+
+
+# In[64]:
+
+
+a=b
+print(a,b)
+
+
+# In[65]:
+
+
+2 == "2"
+
+
+# In[69]:
+
+
+s='amer'
+str(s) == s     
+      
+
+True == 1
+# In[70]:
+
+
+print('true')
+
+
+# In[79]:
+
+
+s=' '
+
+
+# In[82]:
+
+
+2 and 1
+
+
+# In[83]:
+
+
+2 or 1
+
+
+# In[84]:
+
+
+1 and 1
+
+
+# In[85]:
+
+
+0 and 1
+
+
+# In[ ]:
+
+
+
+
